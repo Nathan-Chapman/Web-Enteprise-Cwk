2eac40bbeef881398d3aa7b8b673757ca38babdf
Nathan-Chapman <47510759+Nathan-Chapman@users.noreply.github.com>
2021-04-14 23:07:40 +0100

    Added email and password to signup

--- src/main/java/nathanchapman/cwk3/ent/Person.java
+++ src/main/java/nathanchapman/cwk3/ent/Person.java
@@ -24,6 +24,24 @@ public class Person implements Serializable {
     private Long id;
     private String firstName;
     private String lastName;
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     private boolean adminStatus;
     private String nationalFederation;
     private String userRole;
