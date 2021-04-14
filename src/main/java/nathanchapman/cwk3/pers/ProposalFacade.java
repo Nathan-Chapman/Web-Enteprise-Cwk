057b961da91e49f4d7bf5810a36fbcad6a38bf92
Nathan-Chapman <47510759+Nathan-Chapman@users.noreply.github.com>
2021-04-14 23:11:04 +0100

    Added proposals entity // Not linked

---
+++ src/main/java/nathanchapman/cwk3/pers/ProposalFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nathanchapman.cwk3.pers;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import nathanchapman.cwk3.ent.Proposal;
+
+/**
+ *
+ * @author natha
+ */
+@Stateless
+public class ProposalFacade extends AbstractFacade<Proposal> {
+
+    @PersistenceContext(unitName = "RelationsPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ProposalFacade() {
+        super(Proposal.class);
+    }
+    
+}
