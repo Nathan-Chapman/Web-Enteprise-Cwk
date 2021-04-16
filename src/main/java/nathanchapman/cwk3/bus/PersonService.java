bc4cc73a0b44015197030e4b1709569010f6d3c2
Nathan-Chapman <47510759+Nathan-Chapman@users.noreply.github.com>
2021-04-16 18:29:24 +0100

    Added getProposalsByPersonId and getVotesByPersonId

--- src/main/java/nathanchapman/cwk3/bus/PersonService.java
+++ src/main/java/nathanchapman/cwk3/bus/PersonService.java
@@ -5,11 +5,16 @@
  */
 package nathanchapman.cwk3.bus;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import nathanchapman.cwk3.ent.Person;
+import nathanchapman.cwk3.ent.Proposal;
+import nathanchapman.cwk3.ent.Vote;
 import nathanchapman.cwk3.pers.PersonFacade;
 import nathanchapman.cwk3.pers.ProposalFacade;
+import nathanchapman.cwk3.pers.VoteFacade;
 
 /**
  *
@@ -22,10 +27,34 @@ public class PersonService {
     private PersonFacade pf;
     @EJB
     private ProposalFacade propf;
+    @EJB
+    private VoteFacade vf;
 
     public Person createNewPerson(Person p) {
         p.setAdminStatus(false);
         pf.create(p);
         return p;
+    }
+    
+        public List<Proposal> getProposalByUser(long id) {
+        List<Proposal> allProposal = propf.findAll();
+        List<Proposal> temp = new ArrayList<>();
+        for (int i=0 ; i < allProposal.size() ; i++) {
+            if (allProposal.get(i).getPerson().getId() == id) {            
+               temp.add(allProposal.get(i));
+            }     
+        }
+        return temp;
+    }
+        
+            public List<Vote> getVotesByPersonId(long id) {
+        List<Vote> allVotes = vf.findAll();
+        List<Vote> temp = new ArrayList<>();
+        for (int i=0 ; i < allVotes.size() ; i++) {
+            if (allVotes.get(i).getPerson().getId() == id) {            
+               temp.add(allVotes.get(i));
+            }     
+        }
+        return temp;
     }
 }
