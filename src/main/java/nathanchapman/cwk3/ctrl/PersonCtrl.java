8ce06b94c465fe25c7d36ecf342837b87004d373
Nathan-Chapman <47510759+Nathan-Chapman@users.noreply.github.com>
2021-04-18 03:36:23 +0100

    Merged propCtrl personCtrl voteCtrl to session scope
    Couldnt figure out log in sessions so did this

--- src/main/java/nathanchapman/cwk3/ctrl/PersonCtrl.java
+++ src/main/java/nathanchapman/cwk3/ctrl/PersonCtrl.java
@@ -6,12 +6,19 @@
 package nathanchapman.cwk3.ctrl;
 
 import java.io.Serializable;
-import javax.annotation.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import nathanchapman.cwk3.bus.PersonService;
+import nathanchapman.cwk3.bus.ProposalService;
+import nathanchapman.cwk3.bus.VoteService;
 import nathanchapman.cwk3.ent.Person;
+import nathanchapman.cwk3.ent.Proposal;
+import nathanchapman.cwk3.ent.Vote;
 
 @Named(value = "personCtrl")
 @SessionScoped
@@ -20,7 +27,74 @@ public class PersonCtrl implements Serializable {
     private Person p = new Person();
     private String email;
     private String password; //Shouldnt store it in plain text
+    
+    private Proposal prop = new Proposal();
+    private long id;
+    List<Proposal> allProp = new ArrayList<>();
 
+    private Vote vote= new Vote();
+    private long propId;
+    private long personId;
+    
+    private List<Vote> allVotes = new ArrayList<>();
+    private List<Vote> votes = new ArrayList<>();
+    private long pId = 2;
+    
+    public List<Vote> getVotes() {
+        if (votes.isEmpty()) {
+            votes = vs.getVotesByProposalId(pId);
+        }
+        votes = vs.getVotesByProposalId(pId);
+        return votes;
+    }
+
+    public void setVotes(List<Vote> votes) {
+        this.votes = votes;
+    }
+
+    public long getqId() {
+        return pId;
+    }
+
+    public void setqId(long pId) {
+        this.pId = pId;
+    }
+    
+    public Vote getVote() {
+        return vote;
+    }
+
+    public void setVote(Vote vote) {
+        this.vote = vote;
+    }
+
+    public long getPropId() {
+        return propId;
+    }
+
+    public void setPropId(long propId) {
+        this.propId = propId;
+    }
+
+    public long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(long personId) {
+        this.personId = personId;
+    }
+
+    public List getAllVotes() {
+        if (allVotes.isEmpty()) {
+            allVotes = vs.getAllVotes();       
+        }
+        return allVotes;
+    }
+
+    public void setAllVotes(List<Vote> allVotes) {
+        this.allVotes = allVotes;
+    }
+    
     public String getEmail() {
         return email;
     }
@@ -47,12 +121,63 @@ public class PersonCtrl implements Serializable {
     
     public PersonCtrl() {
     }
+    public List<Proposal> getAllProp() {
+        if (allProp.isEmpty()) {
+            allProp = props.getAllProposals();
+        }
+        return allProp;
+    }
+
+    public void setAllProp(List<Proposal> allProp) {
+        this.allProp = allProp;
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    public Proposal getProp() {
+        return prop;
+    }
+    
+    public void setProp(Proposal prop) {
+        this.prop = prop;
+    }
     
     @EJB
     private PersonService ps;
+    @EJB
+    private ProposalService props;
+     @EJB
+    private VoteService vs;
 
     public String doCreatePerson() {
         ps.createNewPerson(p);
+        return "";
+    }
+    
+    public String doCreateProposal() {
+        props.createNewProposal(prop, id);
+        return"";
+    }
+    
+    public String changeProposal() {
+        props.changeProposal(prop, id);
+        return"";
+    }
+    
+    public String deleteProposal() {
+        props.changeProposal(prop, id);
+        return"";
+    }
+    
+    public String displayProposalById() {
+        Map<String, String>  res = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        setId(Long.valueOf(res.get("id")));
+        setProp(props.getPropById(id));
         return "";
     }
     
@@ -69,4 +194,13 @@ public class PersonCtrl implements Serializable {
         this.logInResult = logInResult;
     }
     
+    public String doCreateVote() {
+        vs.createNewVote(vote, propId, personId);
+        return"";
+    }
+    
+    public String changeVote() {
+        vs.changeVote(vote, propId, personId);
+        return"";
+    }
 }
